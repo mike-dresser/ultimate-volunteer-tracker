3d57558c0df6e2b0eb160c9ef6b5f2898cdde6c6
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-03-04 14:43:57 -0500

    created state for search and defined variable for searchedVolunteers

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-function Search() {
+function Search({search, setSearch}) {
   return (
-    <input className="search" type="text" placeholder="Search Volunteers" />
+    <input 
+    className="search" 
+    type="text" 
+    placeholder="Search Volunteers" 
+    value={search}
+    onChange={(e)=>setSearch(e.target.value)} />
   );
 }
 
