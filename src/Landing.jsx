7895197261c0cc6dfbc88deff9ae5d4fa611984c
sydneyfriedman <friedman.sydneyl@gmail.com>
2024-03-05 16:25:08 -0500

    took out the css for the background image

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import LogIn from './LogIn';
-import video from './assets/video.mp4';
+
 
 
 function Landing() {
   return(
     <>
-    <div className='video'>
+    {/* <div className='video'>
       <video src={video} autoPlay loop muted />
-    </div>
-      {/* <img id='html-background-image' alt="Ultimate Community" src='https://d36m266ykvepgv.cloudfront.net/uploads/media/zFWfHuiqin/s-1170-878/img-2326.jpg'/> */}
+    </div> */}
+      <img id='html-background-image' alt="Ultimate Community" src='https://d36m266ykvepgv.cloudfront.net/uploads/media/zFWfHuiqin/s-1170-878/img-2326.jpg'/>
       <div className='landingText'>
         <h1>DiscNY Volunteer Tracker</h1>
         <p>Use your volunteer hours to pay for play!</p>
