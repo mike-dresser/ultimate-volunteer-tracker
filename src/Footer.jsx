57cf208ff84a480c03d18d33c7374b033e1d8298
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-06 12:39:51 -0500

    Add footerItem component for tooltips

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import FooterItem from './FooterItem';
 
 function Footer() {
   return (
     <footer>
-      <Link to="https://discny.org/" >
-        <img src="/src/assets/discnyny.png" alt= "DiscNY Homepage"/>
-      </Link>
-      <Link to="https://instagram.com/discnewyork">
-        <img src="/src/assets/1_Instagram_colored_svg_1-128.webp" alt= "Instagram"/>
-      </Link>
-      <Link to="https://linktr.ee/discny">
-        <img src="/src/assets/linktree.png" alt= "linktr"/>
-      </Link>
-      <Link to="https://join.slack.com/t/discny/shared_invite/zt-277kqbgbp-SQfmKL1dPKVCXMMa9wucBQ">
-        <img src="/src/assets/306_Slack_logo-64.webp" alt="Slack"/>
-      </Link>
-      <Link to="https://discny.org/newsletters">
-        <img src="/src/assets/vector_652_01-64.webp" alt= "Newsletter"/>
-      </Link>
-      <Link to="info@discny.org">
-        <img src="/src/assets/donate.webp" alt= "Donate"/>
-      </Link>
-      <Link to="https://discny.org/s/category/donation">
-        <img src="/src/assets/email_mail__letter__internet__envelope__chat__1_-64.webp" alt="Contact" />
-      </Link>
+      <FooterItem
+        url="https://discny.org/"
+        imgSrc="/src/assets/discnyny.png"
+        hoverText="DiscNY Homepage"
+      />
+      <FooterItem
+        url="https://instagram.com/discnewyork"
+        imgSrc="/src/assets/1_Instagram_colored_svg_1-128.webp"
+        hoverText="Instagram"
+      />
+      <FooterItem
+        url="https://linktr.ee/discny"
+        imgSrc="/src/assets/linktree.png"
+        hoverText="linktr"
+      />
+      <FooterItem
+        url="https://join.slack.com/t/discny/shared_invite/zt-277kqbgbp-SQfmKL1dPKVCXMMa9wucBQ"
+        imgSrc="/src/assets/306_Slack_logo-64.webp"
+        hoverText="Slack"
+      />
+      <FooterItem
+        url="https://discny.org/newsletters"
+        imgSrc="/src/assets/vector_652_01-64.webp"
+        hoverText="Newsletter"
+      />
+      <FooterItem
+        url="https://discny.org/s/category/donation"
+        imgSrc="/src/assets/donate.webp"
+        hoverText="Donate"
+      />
+      <FooterItem
+        url="info@discny.org"
+        imgSrc="/src/assets/email_mail__letter__internet__envelope__chat__1_-64.webp"
+        hoverText="Contact"
+      />
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
