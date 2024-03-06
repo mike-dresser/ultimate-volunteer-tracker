d4f8670c4838ce753586c10e34ded2675f487a10
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-06 14:43:34 -0500

    Volunteer form edited, formatted

--- src/VolunteerForm.jsx
+++ src/VolunteerForm.jsx
@@ -1,85 +1,90 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 function VolunteerForm() {
+  const [newVolunteer, setNewVolunteer] = useState({
+    name: '',
+    pronouns: '',
+    image: '',
+    role: '',
+    email: '',
+    pointEntries: [],
+  });
 
-  const [newVolunteer, setNewVolunteer] = useState({           
-  "name" : "",
-  "pronouns" : "",
-  "image" : "",
-  "role" : "",
-  "email" : "",
-  "pointEntries" : [] },
-   )
+  function handleSubmit(e) {
+    e.preventDefault();
 
-   
-   function handleSubmit(e) {
-     e.preventDefault()
-     
-     console.log(newVolunteer)
+    console.log(newVolunteer);
 
-     fetch("http://localhost:3000/users", {
-     method: "POST",
-     headers: {
-      "Content-Type" : "Application/JSON"
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/JSON',
       },
       body: JSON.stringify(newVolunteer),
-     })
+    });
     //     .then((res)=>res.json())
     //     .then((data)=>setNewVolunteer([...newVolunteer, data]));
-
-    
-   }
-     
+  }
 
   return (
-    <div className='newVolunteer'>
+    <div className="newVolunteer">
       <h1>New Volunteer</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter Full Name: </label>
+        <label htmlFor="name">Full Name </label>
         <input
-          type="text" 
-          name="name" 
-          placeholder="Johnny Disc" 
-          value={newVolunteer.name} 
-          onChange={(e)=>setNewVolunteer({...newVolunteer, name: e.target.value})}>
-        </input>
-        <br></br>
-        <label htmlFor="pronouns">Enter Pronouns: </label>
+          type="text"
+          name="name"
+          placeholder="Johnny Disc"
+          value={newVolunteer.name}
+          onChange={(e) =>
+            setNewVolunteer({ ...newVolunteer, name: e.target.value })
+          }
+        ></input>
+
+        <label htmlFor="pronouns">Pronouns </label>
         <input
-          type="text" 
-          name="pronouns" 
-          placeholder="(They/Them)" 
-          value={newVolunteer.pronouns} 
-          onChange={(e)=>setNewVolunteer({...newVolunteer, pronouns: e.target.value})}>
-        </input>
-        <br></br>
-        <label htmlFor="role">Enter Org Role: </label>
+          type="text"
+          name="pronouns"
+          placeholder="(They/Them)"
+          value={newVolunteer.pronouns}
+          onChange={(e) =>
+            setNewVolunteer({ ...newVolunteer, pronouns: e.target.value })
+          }
+        ></input>
+
+        <label htmlFor="role">Org Role </label>
         <input
-          type="text" 
-          name="role" 
-          placeholder="General" 
-          value={newVolunteer.role} 
-          onChange={(e)=>setNewVolunteer({...newVolunteer, role: e.target.value})}>
-        </input>
-        <br></br>
-        <label htmlFor="email">Enter Email: </label>
+          type="text"
+          name="role"
+          placeholder="General"
+          value={newVolunteer.role}
+          onChange={(e) =>
+            setNewVolunteer({ ...newVolunteer, role: e.target.value })
+          }
+        ></input>
+
+        <label htmlFor="email">Email </label>
         <input
-          type="text" 
-          name="email" 
-          placeholder="johnny.disc@discny.org" 
-          value={newVolunteer.email} 
-          onChange={(e)=>setNewVolunteer({...newVolunteer, email: e.target.value})}>
-        </input>
-        <br></br>
-        <label htmlFor="image">Enter Image URL: </label>
+          type="text"
+          name="email"
+          placeholder="johnny.disc@discny.org"
+          value={newVolunteer.email}
+          onChange={(e) =>
+            setNewVolunteer({ ...newVolunteer, email: e.target.value })
+          }
+        ></input>
+
+        <label htmlFor="image">Image URL </label>
         <input
-          type="text" 
-          name="image" 
-          placeholder="Image URL" 
-          value={newVolunteer.image} 
-          onChange={(e)=>setNewVolunteer({...newVolunteer, image: e.target.value})}>
-        </input>
-        <br></br>
+          type="text"
+          name="image"
+          placeholder="http:// ... .jpg"
+          value={newVolunteer.image}
+          onChange={(e) =>
+            setNewVolunteer({ ...newVolunteer, image: e.target.value })
+          }
+        ></input>
+
         <button type="submit">Add Volunteer</button>
       </form>
     </div>
